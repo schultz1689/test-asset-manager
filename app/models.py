a182b83a6e7e781b786696c85359f68887df0a33
schultz1689 <reschultz1014@gmail.com>
2025-11-22 14:49:27 -0600

    extending domain to include SimulationConfigs and TestRuns

--- app/models.py
+++ app/models.py
@@ -1,13 +1,116 @@
 # app/models.py
 
-from sqlalchemy import Column, Integer, String, Text
+from datetime import datetime
+import enum
+
+from sqlalchemy import ( 
+    Column,
+    Integer,
+    String,
+    Text,
+    ForeignKey,
+    DateTime,
+    Enum,
+)
+from sqlalchemy.orm import relationship
+
 from .db import Base
 
 class Testbed(Base):
+    """
+    ORM model for testbeds. 
+
+    Each Testbed is at least the hardware required to run a sim. This includes devices that run remotely.
+    """
 
     __tablename__ = "testbeds"
 
     id = Column(Integer, primary_key=True, index=True)
+    # The hostname of the device. If dual host, use the first hostname and clarify in the description
     name = Column(String(100), nullable=False, unique=True, index=True)
     location = Column(String(100), nullable=True)
     description = Column(Text, nullable=True)
+
+    configs = relationship(
+        "SimulationConfig",
+        back_populates="testbed",
+        cascade="all, delete-orphan",
+    )
+
+class SimulationConfig(Base):
+
+    """
+    ORM model for the simulation_configs table
+
+    Represents the current configuration that a Testbed is supporting, and has supported in the past.
+    """
+
+    __tablename__ = "simulation_configs"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    testbed_id = Column(
+        Integer,
+        ForeignKey("testbeds.id"),
+        nullable=False,
+        index=True,
+    )
+
+    # Right now the idea is that we can track every single SimulationConfig ran on a specific device, and what its current configuration is.
+    # As of making this, I belive this will change a lot. For now lets get this prototype off the ground.
+    name = Column(String(100), nullable=False)
+    sim_version = Column(String(50), nullable=True)
+    os = Column(String(50), nullable=True)
+    notes = Column(Text, nullable=True)
+    is_current_config = Column(bool, nullable=True)
+
+    testbed = relationship(
+        "Testbed",
+        back_populates="configs",
+    )
+
+    runs = relationship(
+        "TestRun",
+        back_populates="config",
+        cascade="all, delete-orphan",
+    )
+
+class RunResult(str, enum.Enum):
+    """
+    Enum of allowed run results.
+    """
+    PASS = "PASS"
+    FAIL = "FAIL"
+    IN_PROGRESS = "IN_PROGRESS"
+class TestRun(Base):
+    """
+    ORM model for the test_runs tablee
+
+    Represents a single execution of a SimulationConfig
+    """
+
+    __tablename__ = "test_runs"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    simulation_config_id = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.timezone.utc,
+    )
+
+    operator = Column(String(100), nullable=True)
+
+    result = Column(
+        Enum(RunResult),
+        nullable=False,
+        default=RunResult.IN_PROGRESS,
+    )
+
+    notes = Column(Text, nullable = True)
+
+    config = relationship(
+        "SimulationConfig",
+        back_populates="runs",
+        cascade="all, delete-orphan",
+    )
--- app/schemas.py
+++ app/schemas.py
@@ -1,11 +1,18 @@
 # app/schemas.py
 
+from datetime import datetime
+from typing import Optional
+
 from pydantic import BaseModel
+
+from .models import RunResult
+
 
 class TestbedBase(BaseModel):
     name: str
     location: str | None = None
     description: str | None = None
+
 
 class TestbedCreate(TestbedBase):
     """
@@ -13,11 +20,68 @@ class TestbedCreate(TestbedBase):
     """
     pass
 
+
 class TestbedRead(TestbedBase):
     """
     Schema for reading a Testbed from the API.
     """
     id: int
+
+    class Config:
+        orm_mode = True
+
+
+class SimulationConfigBase(BaseModel):
+    """
+    Common fields shared by SimulationConfig input/output models
+    """
+    testbed_id: int
+    name: str
+    software_version: Optional[str] = None
+    os: Optional[str] = None
+    notes: Optional[str] = None 
+
+
+class SimulationConfigCreate(SimulationConfigBase):
+    """
+    Schema for creating a new SimulationCOnfig.
+    """
+    pass 
+
+
+class SimulationConfigRead(SimulationConfigBase):
+    """
+    Schema for reading a SimulationConfig from the API
+    """
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class TestRunBase(BaseModel):
+    """
+    Common fields shared by TestRun input/output models.
+    """
+    simulation_config_id: int
+    operator: Optional[str] = None
+    result: RunResult = RunResult.IN_PROGRESS
+    notes: Optional[str] = None
+
+
+class TestRunCreate(TestRunBase):
+    """
+    Schema for creating a new TestRun.
+    """
+    pass
+
+
+class TestRunRead(TestRunBase):
+    """
+    Schema for reading a TestRun from the API.
+    """
+    id: int
+    run_date: datetime
 
     class Config:
         orm_mode = True
